3aba1c81a05fb2b1786870992801bc3c3c9b5686
Emre Baskan <3mr3baskan@gmail.com>
2018-12-16 11:24:50 +0300

    route

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import Pokemons from "./components/Pokemons/Pokemons";
+import AddPokemon from "./components/Pokemons/AddPokemon/AddPokemon";
+
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+
+import { Provider } from 'react-redux';
+import store from './store/store';
+import EditPokemon from "./components/Pokemons/EditPokemon/EditPokemon";
+
 
 class App extends Component {
   render() {
     return (
-        <React.Fragment>
-          <Navbar brand={'Pokedex'}/>
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                  <Pokemons/>
-              </div>
-            </div>
+        <Provider store={store}>
+            <Router>
+                <Navbar brand={'Pokedex'}/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <Switch>
+                                <Route exact path={'/'} component={Pokemons} />
+                                <Route exact path={'/pokemons/add'} component={AddPokemon}/>
+                                <Route exact path={'/pokemons/edit/:id'} component={EditPokemon} />
+                                <Route exact path={'/about'} component={About} />
+                                <Route component={NotFound}/>
+                            </Switch>
+                        </div>
+                    </div>
 
-          </div>
-        </React.Fragment>
+                </div>
+            </Router>
+        </Provider>
     );
   }
 }
---
+++ src/components/Pokemons/EditPokemon/EditPokemon.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class EditPokemon extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default EditPokemon;
